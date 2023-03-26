185cc5411c43cb83135f1918ae6364fdd510d80e
eldeeb91 <eldeeb.abdelhamed@gmail.com>
2023-03-26 20:13:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
+echo "Installing needed libs"
 sudo pip3 install Adafruit_BBIO
+echo "Copy files"
 sudo cp ./src/CPU_FAN_BBB.py /usr/lib/CPU_FAN_BBB/CPU_FAN_BBB.py
 sudo cp ./CPU_FAN_BBB.service /lib/systemd/system/CPU_FAN_BBB.service
 
 sudo systemctl daemon-reload
+echo "Enabling the service"
 sudo systemctl enable CPU_FAN_BBB.service
+echo "Starting the service"
 sudo systemctl start CPU_FAN_BBB.service
+echo "Checking the service"
 sudo systemctl status CPU_FAN_BBB.service
